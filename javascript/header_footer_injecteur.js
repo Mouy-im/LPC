5433d3eb4bd1e2ed3c736b12ffdd225da23fc22f
Mouy-Im GIBASSIER <mouyim.gibassier@hotmail.com>
2020-12-07 16:12:58 +0100

    ajout du logo dans le navigateur

--- javascript/header_footer_injecteur.js
+++ javascript/header_footer_injecteur.js
@@ -5,6 +5,10 @@ $(function() {
             $('.div_search').toggle('slow');
         });
 
+        /*$(document).on('click', '.nav-item', function() {
+            $(this).children().toggle('slow');
+        });*/
+
         $(document).on('click', '.user', function() {
             $('.user_ul').toggle('slow');
         });
@@ -22,41 +26,39 @@ $(function() {
 
         $(document).on('click', '.untiers>h2', function() {
             $(this).siblings().slideToggle('slow');
-        });
 
-        $(document).on('click', '.fa-print', function() {
-            window.print();
         });
 
         const retour = document.querySelector('#basdepage .retour-haut');
         window.onscroll = function() {
 
-            //header -lg-
-            if (window.innerWidth > 992 && document.documentElement.scrollTop > 80) {
-                retour.classList.add('retour_scroll');
+            if (document.documentElement.scrollTop > 150 || (window.innerWidth <= 768 && document.documentElement.scrollTop > 0)) {
                 document.querySelector('#offre').classList.add('scroll_hide');
-                document.querySelector('div.navbar').style.boxShadow = '0px 10px 10px -10px grey';
                 document.querySelector('.logo_menusearch').classList.add('scroll_hide');
-                document.querySelector('.menu_principal').style.borderTop = 'none';
-                document.querySelector('.menu_principal').style.borderBottom = 'none';
+                retour.classList.add('retour_scroll');
                 document.querySelector('.menu_principal').style.boxShadow = '0px 10px 10px -10px grey';
                 document.querySelector('.logo_on_scroll').style.display = 'block';
+                document.querySelector('.menu_principal').style.borderTop = 'none';
+                document.querySelector('.menu_principal').style.borderBottom = 'none';
+                document.querySelector('div.navbar').style.boxShadow = '0px 10px 10px -10px grey';
                 if (document.querySelector('.menu_gauche')) {
-                    document.querySelector('.menu_gauche').style.top = "20px";
+                    document.querySelector('.menu_gauche').style.top = "10px";
                 }
                 if (document.querySelector('.nav-aside')) {
-                    document.querySelector('.nav-aside').style.top = "720px";
+                    document.querySelector('.nav-aside').style.top = "710px";
                 }
-            } else {
+            } else if (window.innerWidth <= 768 && document.documentElement.scrollTop == 0) {
                 retour.classList.remove('retour_scroll');
-                document.querySelector('#offre').classList.remove('scroll_hide');
+            } else {
 
-                document.querySelector('div.navbar').style.boxShadow = 'none';
+                document.querySelector('#offre').classList.remove('scroll_hide');
                 document.querySelector('.logo_menusearch').classList.remove('scroll_hide');
-                document.querySelector('.menu_principal').style.borderTop = '1px solid grey';
-                document.querySelector('.menu_principal').style.borderBottom = '1px solid grey';
+                retour.classList.remove('retour_scroll');
                 document.querySelector('.menu_principal').style.boxShadow = 'none';
                 document.querySelector('.logo_on_scroll').style.display = 'none';
+                document.querySelector('.menu_principal').style.borderTop = '1px solid grey';
+                document.querySelector('div.navbar').style.boxShadow = 'none';
+                document.querySelector('.menu_principal').style.borderBottom = '1px solid grey';
                 if (document.querySelector('.menu_gauche')) {
                     document.querySelector('.menu_gauche').style.top = "170px";
                 }
@@ -64,34 +66,19 @@ $(function() {
                     document.querySelector('.nav-aside').style.top = "870px";
                 }
             }
+            if (window.innerWidth <= 991 && document.documentElement.scrollTop < 150) {
 
-            //header -md-
-            if ((window.innerWidth <= 991 && window.innerWidth > 768) && document.documentElement.scrollTop > 80) {
-                retour.classList.add('retour_scroll');
-                document.querySelector('#offre').classList.add('scroll_hide');
-                document.querySelector('div.navbar').style.boxShadow = '0px 10px 10px -10px grey';
-                document.querySelector('.menu_principal').style.borderTop = 'none';
-                document.querySelector('.menu_principal').style.borderBottom = 'none';
-
-            } else {
-                document.querySelector('div.navbar').style.boxShadow = 'none';
-
-            }
-
-            //header -sm- -xs-
-            if (window.innerWidth <= 768 && document.documentElement.scrollTop > 0) {
-                retour.classList.add('retour_scroll');
-                document.querySelector('#offre').classList.add('scroll_hide');
-                document.querySelector('div.navbar').style.boxShadow = '0px 10px 10px -10px grey';
-                document.querySelector('.menu_principal').style.borderTop = 'none';
-                document.querySelector('.menu_principal').style.borderBottom = 'none';
-            } else if (window.innerWidth <= 768 && document.documentElement.scrollTop == 0) {
-                retour.classList.remove('retour_scroll');
-                document.querySelector('#offre').classList.remove('scroll_hide');
-
+                document.querySelector('.logo_on_scroll').style.display = 'none';
+            } else if (window.innerWidth <= 991 && document.documentElement.scrollTop > 150) {
+                document.querySelector('.logo_on_scroll').style.display = 'none';
             }
 
         }
+
+        $(document).on('click', '.fa-print', function() {
+            window.print();
+        });
+
     });
     //aside
     if ('.nav-aside') {
@@ -109,4 +96,5 @@ $(function() {
 
 
     }
+
 })
